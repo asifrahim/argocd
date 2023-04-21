031d9fd6ab636f3bd4645e3a83dd06a609c6c422
mrahim <mrahim@elm.sa>
2023-04-22 00:14:33 +0300

    Modified Port Settings for Project

---
+++ complete/src/main/java/com/example/springboot/ServerPortCustomizer.java
@@ -0,0 +1,17 @@
+package com.example.springboot;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class ServerPortCustomizer
+        implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        factory.setPort(8086);
+    }
+
+}
